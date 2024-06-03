992790a4207b01391ccf57000fe40eb9bb8f8fb4
Shanmugham <shanmughams@gmail.com>
2024-06-03 21:40:03 +0530

    with weekends excluded

--- user-hour.sql
+++ user-hour.sql
@@ -1,3 +1,37 @@
+WITH week_data AS (
+    SELECT 
+        soeid,
+        CASE 
+            WHEN date_captured BETWEEN TO_DATE('2024-04-23', 'YYYY-MM-DD') AND TO_DATE('2024-04-29', 'YYYY-MM-DD') THEN 'week1'
+            WHEN date_captured BETWEEN TO_DATE('2024-04-30', 'YYYY-MM-DD') AND TO_DATE('2024-05-06', 'YYYY-MM-DD') THEN 'week2'
+            WHEN date_captured BETWEEN TO_DATE('2024-05-07', 'YYYY-MM-DD') AND TO_DATE('2024-05-13', 'YYYY-MM-DD') THEN 'week3'
+            WHEN date_captured BETWEEN TO_DATE('2024-05-14', 'YYYY-MM-DD') AND TO_DATE('2024-05-20', 'YYYY-MM-DD') THEN 'week4'
+            WHEN date_captured BETWEEN TO_DATE('2024-05-21', 'YYYY-MM-DD') AND TO_DATE('2024-05-25', 'YYYY-MM-DD') THEN 'week5'
+        END AS week
+    FROM 
+        dta
+    WHERE 
+        date_captured BETWEEN TO_DATE('2024-04-23', 'YYYY-MM-DD') AND TO_DATE('2024-05-25', 'YYYY-MM-DD')
+        AND TO_CHAR(date_captured, 'DY') NOT IN ('SAT', 'SUN')
+),
+soeid_week_count AS (
+    SELECT 
+        soeid,
+        COUNT(DISTINCT week) AS week_count
+    FROM 
+        week_data
+    GROUP BY 
+        soeid
+)
+SELECT 
+    soeid
+FROM 
+    soeid_week_count
+WHERE 
+    week_count <= 3;
+
+
+---
 SELECT 
     soeid,
     SUM(CASE 
